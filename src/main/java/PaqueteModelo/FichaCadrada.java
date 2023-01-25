44b736ad61cd6dc13d9c25af14ceb55a7dcedda6
Etutam1 <matute1172@gmail.com>
2023-01-25 10:15:46 +0100

    New Cadrados Clase FichaCadrada

--- src/main/java/PaqueteModelo/FichaCadrada.java
+++ src/main/java/PaqueteModelo/FichaCadrada.java
@@ -4,6 +4,8 @@
  */
 package PaqueteModelo;
 
+import java.awt.Color;
+
 /**
  *
  * @author matut
@@ -12,6 +14,10 @@ public class FichaCadrada extends Ficha{
     
     //CONSTRUCTOR
     public FichaCadrada() {
+        Cadrado cadrado1 = new Cadrado(0,1,Color.RED);
+        Cadrado cadrado2 = new Cadrado(1,1,Color.RED);
+        Cadrado cadrado3 = new Cadrado(1,1,Color.RED);
+        Cadrado cadrado4 = new Cadrado(1,1,Color.RED);
     }
     
     //METODOS
