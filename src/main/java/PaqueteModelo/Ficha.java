c1dc650bd586d42fa85a78e4421a07b5bbad67e2
Etutam1 <matute1172@gmail.com>
2023-02-01 14:29:59 +0100

    Rename VentanaPrincipal

--- src/main/java/PaqueteModelo/Ficha.java
+++ src/main/java/PaqueteModelo/Ficha.java
@@ -20,7 +20,7 @@ public abstract class Ficha {
 
     //CONSTRUCTOR
     public Ficha(Xogo xogo) {
-        this.xogo=xogo;
+        this.xogo = xogo;
     }
 
     //SETTER AND GETTER
@@ -45,18 +45,30 @@ public abstract class Ficha {
 
     public boolean moverAbaixo() {
         iterator = cadrados.iterator();
-        
-        while( iterator.hasNext()){
+        boolean flag = true;
+        while (iterator.hasNext()) {
             Cadrado c = iterator.next();
-                c.lblCadrado.setLocation(c.lblCadrado.getX(), c.lblCadrado.getY()+Xogo.LADO_CADRADO);     
+            if (c.lblCadrado.getY() <= xogo.ventanaPrincipal.getPanelJuego().getHeight() - (Xogo.LADO_CADRADO) * 2) {
+                c.lblCadrado.setLocation(c.lblCadrado.getX(), c.lblCadrado.getY() + Xogo.LADO_CADRADO);        
+            } else {
+                xogo.cadradosChan.add(c);
+                iterator.remove();
+                
+                
+                
+                
+                
+                
+                //cadrados.removeAll(cadrados);
+            } 
         }
-        
+        for (int i = 0; i < xogo.cadradosChan.size(); i++) {
+                    xogo.cadradosChan.get(i).lblCadrado.setLocation(xogo.cadradosChan.get(i).lblCadrado.getX(),  700);
+                }
+
         return true;
     }
 
     public abstract boolean rotar();
 
-    
-    }
-
-
+}
